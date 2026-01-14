63743d4287559cc11159af425e9e2bee32636e85
267nuku <267nuku@gmail.com>
2026-01-14 07:16:11 +0000

    변경사항

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/project_nukuenco/', // CHANGE THIS: Set
+        base: '/', // CHANGE THIS: Set
       server: {
         port: 3000,
         host: '0.0.0.0',
